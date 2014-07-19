7cb538e4dcdb06207b4aaae3bf4ead7244fb2095
Smitha Milli <smitha@Smithas-MacBook-Pro.local>
2014-07-19 15:26:31 -0700

    phone rotation

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,11 @@
+var socket = io('http://df49858.ngrok.com:80');
+
+var phoneProps = {
+  'rotX': 0,
+  'rotY': 0,
+  'rotZ': 0,
+}
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
@@ -6,14 +14,21 @@ renderer.setClearColorHex(0xffffff, 1);
 document.body.appendChild(renderer.domElement);
 var geometry = new THREE.BoxGeometry(4.87, 2.31, 0.30);
 var material = new THREE.MeshBasicMaterial({color: 0x000000});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+var phone = new THREE.Mesh(geometry, material);
+scene.add(phone);
 camera.position.z = 5;
+
 var render = function () {
   requestAnimationFrame(render);
-  cube.rotation.x += 0.1;
-  cube.rotation.y += 0.1;
-  cube.rotation.z += 0.1;
+  phone.rotation.x = phoneProps.rotX;
+  phone.rotation.y = phoneProps.rotY;
+  phone.rotation.z = phoneProps.rotZ;
   renderer.render(scene, camera);
 };
 render();
+
+socket.on('update rotation', function(data) {
+  phoneProps.rotX = data.x;
+  phoneProps.rotY = data.y;
+  phoneProps.rotZ = data.z;
+}); 
