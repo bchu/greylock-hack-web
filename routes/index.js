d06ec368f1cc77316515af9a422680666199c8fb
Smitha Milli <smitha@Smithas-MacBook-Pro.local>
2014-07-19 15:06:40 -0700

    emit rotations

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var socketServer = require('./../lib/socket-server');
 var router = express.Router();
 
 /* Update iphone animation */
 router.post('/update', function(req, res) {
   console.log(req.body.roll + ', ' + req.body.yaw + ', ' + req.body.pitch);
+  socketServer.updateAnimation(req.body.roll, req.body.pitch, req.body.yaw); 
   res.end();
 });
 
